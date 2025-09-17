f0eff1607955436186d2dbc663c3ef055925df8f
Dalmas Otieno <dotieno@bluesquarehub.com>
2025-09-18 00:10:30 +0300

    pull latest image that is tagged with platform

--- scripts/update_base_docker_image.sh
+++ scripts/update_base_docker_image.sh
@@ -1,2 +1,5 @@
-docker build -t dalmasbluesquarehub/templates-ci -f Dockerfile.base .
-docker push dalmasbluesquarehub/templates-ci:latest
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  -t dalmasbluesquarehub/templates-ci:latest \
+  -f Dockerfile.base \
+  --push .
