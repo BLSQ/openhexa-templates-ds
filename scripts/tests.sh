7dc2197ef952119e74a374e48c189d2a94aa79dc
Dalmas Otieno <dotieno@bluesquarehub.com>
2025-09-17 23:47:51 +0300

    pulling templates-ci image directly in ci

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # unit and feature tests written using pytest
-docker build -t templates_ci -f Dockerfile .
-docker run -t templates_ci:latest pytest 
+docker build -t tests_image_ci -f Dockerfile .
+docker run -t tests_image_ci:latest pytest 
 status_of_previous_command=$?
 if [[ $status_of_previous_command -ne 0 ]]; then
     # unit tests have failed
