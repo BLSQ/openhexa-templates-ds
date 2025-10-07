7803cb760ea11fe70b3af85c0e2b3f52f7786ab7
Dalmas Otieno <dotieno@bluesquarehub.com>
2025-10-07 16:37:21 +0300

    trying Pytest coverage comment

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # unit and feature tests written using pytest
-docker run -t blsq/openhexa-blsq-environment:latest pytest
+docker run -t blsq/openhexa-blsq-environment:latest pytest --junitxml=pytest.xml --cov-report=term-missing:skip-covered --cov=. tests/ | tee pytest-coverage.txt
 status_of_previous_command=$?
 if [[ $status_of_previous_command -ne 0 ]]; then
     # unit tests have failed
@@ -15,6 +15,3 @@ if [[ $status_of_previous_command -ne 0 ]]; then
     # unit tests have failed
     exit 1
 fi
-
-# Generate Coverage Report
-docker run -t blsq/openhexa-blsq-environment:latest coverage report -m
