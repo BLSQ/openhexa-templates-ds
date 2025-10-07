8f630f42fad848324ef1dec1348825a61f68b229
Dalmas Otieno <dotieno@bluesquarehub.com>
2025-10-07 15:15:23 +0300

    using blsq/openhexa-blsq-environment:latest as the base image

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # unit and feature tests written using pytest
-docker build -t tests_image_ci -f Dockerfile .
-docker run -t tests_image_ci:latest pytest 
+docker run -t blsq/openhexa-blsq-environment:latest pytest
 status_of_previous_command=$?
 if [[ $status_of_previous_command -ne 0 ]]; then
     # unit tests have failed
