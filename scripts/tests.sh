c8f2170c4657aa7794de37e332e5d6d257b672c6
Dalmas Otieno <dotieno@bluesquarehub.com>
2025-10-22 20:19:38 +0300

    relocating pytest

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Run tests inside Docker, mounting both source and output dirs
-docker run --rm -t \
-  -v "$(pwd)":/app \
-  -w /app \
-  blsq/openhexa-blsq-environment:latest pytest
-
-status_of_previous_command=$?
-if [[ $status_of_previous_command -ne 0 ]]; then
-    echo "❌ Unit tests failed"
-    exit 1
-fi
-
 # Run template structure validation
 ./scripts/pipeline_template_contents.sh
 status_of_previous_command=$?
