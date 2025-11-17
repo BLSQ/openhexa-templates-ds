4954acc50e7ea41335afe546d7ed68381f05adc5
Dalmas Otieno <dotieno@bluesquarehub.com>
2025-11-17 21:05:29 +0300

    combine validation config with validation for simple validation configs

--- dhis2_extract_data_elements/validate.py
+++ dhis2_extract_data_elements/validate.py
@@ -1,5 +1,101 @@
 import polars as pl
-from validation_config import expected_columns
+
+expected_columns = [
+    {
+        "name": "data_element_id",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "data_element_name",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "organisation_unit_id",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "category_option_combo_id",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "attribute_option_combo_id",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "category_option_combo_name",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "period",
+        "type": "String",
+        "number of characters": 6,
+        "can be converted to integer": True,
+        "not null": False,
+    },
+    {
+        "name": "value",
+        "type": "String",
+        "number of characters": 2,
+        "can be converted to integer": True,
+        "not null": False,
+    },
+    {
+        "name": "level_1_id",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "level_2_id",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "level_3_id",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "level_4_id",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "level_1_name",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "level_2_name",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "level_3_name",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "level_4_name",
+        "type": "String",
+        "not null": False,
+    },
+    {
+        "name": "created",
+        "type": "Datetime(time_unit='ms', time_zone='UTC')",
+        "not null": False,
+    },
+    {
+        "name": "last_updated",
+        "type": "Datetime(time_unit='ms', time_zone='UTC')",
+        "not null": False,
+    },
+]
 
 
 def validate_data(df: pl.DataFrame) -> None:
