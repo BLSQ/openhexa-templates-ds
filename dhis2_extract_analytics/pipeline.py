e6ff10546761d06f64292a1e36b4c0469044f2ed
Dalmas Otieno <dotieno@bluesquarehub.com>
2025-11-18 13:14:46 +0300

    renaming validation back to validation

--- dhis2_extract_analytics/pipeline.py
+++ dhis2_extract_analytics/pipeline.py
@@ -23,7 +23,7 @@ from openhexa.toolbox.dhis2.dataframe import (
     join_object_names,
 )
 from openhexa.toolbox.dhis2.periods import period_from_string
-from validate_dhis2_extract_analytics import validate_data
+from validate import validate_data
 
 
 @pipeline("dhis2-extract-analytics")
