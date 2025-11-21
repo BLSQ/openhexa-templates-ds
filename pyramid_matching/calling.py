04acef4049cc696bbd1769ff06e03f88bf0b056c
lgarridobsq <lgarrido@bluesquarehub.com>
2025-11-21 14:18:57 +0100

    feat(matching_pyramids): Changes after Esteban's review

--- pyramid_matching/calling.py
+++ pyramid_matching/calling.py
@@ -10,15 +10,16 @@ def main():
     data = pl.read_csv(
         "/home/leyregarrido/01_github_repos/openhexa-templates-ds/pyramid_matching/data_to_match_2.csv"
     )
-    levels_to_match = ["level_2", "level_3", "level_4", "level_5"]
+    # levels_to_match = ["level_2", "level_3", "level_4", "level_5"]
     threshold = 80
     method = "fuzz_ratio"
-    matched_data, data_not_matched, pyramid_not_matched = match_pyramids(
-        data, pyramid, threshold=threshold, method=method
+    matched_data, matched_data_simplified, data_not_matched, pyramid_not_matched = match_pyramids(
+        data, pyramid, threshold=threshold, method=method, matching_col_suffix="_purum"
     )
     print(matched_data.head())
     print(data_not_matched.head())
     print(pyramid_not_matched.head())
+    print(matched_data_simplified.head())
     print("yo")
 
 
--- pyramid_matching/matching_pyramids.py
+++ pyramid_matching/matching_pyramids.py
@@ -9,31 +9,34 @@ def match_pyramids(
     levels_to_match: list | None = None,
     threshold: int = 80,
     method: str = "fuzz_ratio",
-) -> tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame]:
+    matching_col_suffix: str = "_name",
+) -> tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame, pl.DataFrame]:
     """Match the locations in data with the locations in pyramid.
 
-    It returns a dataframe with the matched locations. It will include the name columns,
+    It returns a dataframe with the matched locations. It will include the matching columns,
     other attribute columns from the data or pyramid dataframe (p.e., location IDs),
     the score of the matching and whether the same pyramid location has been matched
     to two different data locations.
 
-    Depeding on the method specified, the names will be matched differently.
+    Depeding on the method specified, the matching columns will be matched differently.
 
     Note that the data and pyramid dataframes should be cleaned.
-    (a) The column names should be (for the level i) level_i_name, level_i_id,
+    (a) The column names should be (for the level i) level_i_matching, level_i_id,
     level_i_attribute1, ...
     (b) If there is any pre-cleaning to do (lowercase, no accents, no special characters, etc.),
     it should be done before calling this function.
+    (c) We will do the matching in the columns that have the suffix specified in
+    matching_col_suffix.
 
     Parameters
     ----------
     data : pl.DataFrame
         The dataframe with the data to be matched.
-        The column names should be (for the level _alpha_) level_alpha_name, level_alpha_id,
+        The column matching should be (for the level _alpha_) level_alpha_matching, level_alpha_id,
         level_alpha_attribute1, ...
     pyramid : pl.DataFrame
         The dataframe with the pyramid to match against.
-        The column names should be (for the level _alpha_) level_alpha_name, level_alpha_id,
+        The column matching should be (for the level _alpha_) level_alpha_matching, level_alpha_id,
         level_alpha_attribute1, ...
     levels_to_match : list
         The list of levels to match. The levels should be in the order from higher to lower
@@ -42,11 +45,14 @@ def match_pyramids(
         The threshold for the matching score. Default is 80.
     method : str, optional
         The method to use for the matching. Default is "fuzz_ratio".
+    matching_col_suffix: str, optional
+        The suffix of the column that we will do the matching on.
 
     Returns
     -------
-    tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame]
-        The dataframes with the matched data, the data not matched, and the pyramid not matched.
+    tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame, pl.DataFrame]
+        The dataframes with the matched data, the simplified matched data,
+        the data not matched, and the pyramid not matched.
     """
     if not isinstance(data, pl.DataFrame):
         data = pl.DataFrame(data)
@@ -57,12 +63,12 @@ def match_pyramids(
     pyramid = pyramid.unique()
 
     if levels_to_match is None:
-        levels_to_match = _get_levels_to_match(data, pyramid)
+        levels_to_match = _get_levels_to_match(data, pyramid, matching_col_suffix)
     else:
-        _check_levels(data, pyramid, levels_to_match)
+        _check_levels(data, pyramid, levels_to_match, matching_col_suffix)
         levels_to_match.sort()
 
-    attributes = _get_attributes(data, pyramid, levels_to_match)
+    attributes = _get_attributes(data, pyramid, levels_to_match, matching_col_suffix)
 
     levels_already_matched = []
     list_data_not_matched = []
@@ -81,6 +87,7 @@ def match_pyramids(
             threshold,
             method,
             attributes[level],
+            matching_col_suffix,
         )
         levels_already_matched.append(level)
         if not data_no_match_level.is_empty():
@@ -97,7 +104,9 @@ def match_pyramids(
 
     repeated_levels_check = []
     for level in levels_to_match:
-        data_matched = _add_repeated_matches(data_matched, level, repeated_levels_check)
+        data_matched = _add_repeated_matches(
+            data_matched, level, repeated_levels_check, matching_col_suffix
+        )
         repeated_levels_check.append(level)
 
     data_not_matched = pl.concat(list_data_not_matched) if list_data_not_matched else pl.DataFrame()
@@ -105,21 +114,23 @@ def match_pyramids(
         pl.concat(list_pyramid_not_matched) if list_pyramid_not_matched else pl.DataFrame()
     )
 
-    data_matched = _reorder_match_columns(data_matched, levels_to_match, attributes)
+    data_matched, data_matched_simplified = _reorder_match_columns(
+        data_matched, levels_to_match, attributes, matching_col_suffix
+    )
 
-    return data_matched, data_not_matched, pyramid_not_matched
+    return data_matched, data_matched_simplified, data_not_matched, pyramid_not_matched
 
 
 def _reorder_match_columns(
-    data: pl.DataFrame, levels_to_match: list, attributes: dict
-) -> pl.DataFrame:
+    data: pl.DataFrame, levels_to_match: list, attributes: dict, matching_col_suffix: str
+) -> tuple[pl.DataFrame, pl.DataFrame]:
     """Re order the columns in the matched dataframe.
 
     We want the columns to be ordered as:
     - For each level to match:
-        - input name
+        - input matching column
         - input attributes
-        - target name
+        - target matching column
         - target attributes
         - score
         - repeated matches indicator
@@ -132,27 +143,38 @@ def _reorder_match_columns(
         The list of levels to match.
     attributes : dict
         The attributes in each level, both for data and pyramid.
+    matching_col_suffix : str
+        The suffix of the column that we will do the matching on.
 
     Returns
     -------
     pl.DataFrame
         The dataframe with the matched data, with the columns reordered.
+    pl.DataFrame
+        The dataframe with the matched data, without any extra columns
     """
-    cols_order = []
+    cols_order_full = []
+    cols_order_simple = []
     for level in levels_to_match:
-        cols_order.append(config.preffix_input_data + level + "_name")
+        cols_order_full.append(config.preffix_input_data + level + matching_col_suffix)
+        cols_order_simple.append(config.preffix_input_data + level + matching_col_suffix)
         for attr in attributes[level]["data"]:
-            cols_order.append(config.preffix_input_data + attr)
-        cols_order.append(config.preffix_target_data + level + "_name")
+            cols_order_full.append(config.preffix_input_data + attr)
+            cols_order_simple.append(config.preffix_input_data + attr)
+        cols_order_full.append(config.preffix_target_data + level + matching_col_suffix)
+        cols_order_simple.append(config.preffix_target_data + level + matching_col_suffix)
         for attr in attributes[level]["pyramid"]:
-            cols_order.append(config.preffix_target_data + attr)
-        cols_order.append("score_" + level)
-        cols_order.append("repeated_matches_" + level)
-    other_cols = [col for col in data.columns if col not in cols_order]
-    return data.select(other_cols + cols_order)
+            cols_order_full.append(config.preffix_target_data + attr)
+            cols_order_simple.append(config.preffix_target_data + attr)
+        cols_order_full.append("score_" + level)
+        cols_order_full.append("repeated_matches_" + level)
+    other_cols = [col for col in data.columns if col not in cols_order_full]
+    return data.select(other_cols + cols_order_full), data.select(cols_order_simple)
 
 
-def _add_repeated_matches(data: pl.DataFrame, level: str, upper_levels: list) -> pl.DataFrame:
+def _add_repeated_matches(
+    data: pl.DataFrame, level: str, upper_levels: list, matching_col_suffix: str
+) -> pl.DataFrame:
     """If we have matched the same target twice to different inputs, mark them.
 
     Parameters
@@ -164,6 +186,8 @@ def _add_repeated_matches(data: pl.DataFrame, level: str, upper_levels: list) ->
     upper_levels : list
         The list of levels above the current level.
         We also need them to match.
+    matching_col_suffix : str
+        The suffix of the column that we will do the matching on.
 
     Returns
     -------
@@ -171,12 +195,12 @@ def _add_repeated_matches(data: pl.DataFrame, level: str, upper_levels: list) ->
         The dataframe with the matched data, with an additional column
         indicating if there were repeated matches for the target.
     """
-    col_pyramid_name = config.preffix_target_data + level + "_name"
-    col_data_name = config.preffix_input_data + level + "_name"
+    col_pyramid_name = config.preffix_target_data + level + matching_col_suffix
+    col_data_name = config.preffix_input_data + level + matching_col_suffix
     col_repeated_matches = "repeated_matches_" + level
-    list_group_by = [config.preffix_target_data + level + "_name" for level in upper_levels] + [
-        col_pyramid_name
-    ]
+    list_group_by = [
+        config.preffix_target_data + level + matching_col_suffix for level in upper_levels
+    ] + [col_pyramid_name]
 
     counts = (
         data.group_by(list_group_by)
@@ -191,7 +215,9 @@ def _add_repeated_matches(data: pl.DataFrame, level: str, upper_levels: list) ->
     ).drop("count")
 
 
-def _get_attributes(data: pl.DataFrame, pyramid: pl.DataFrame, levels_to_match: list) -> dict:
+def _get_attributes(
+    data: pl.DataFrame, pyramid: pl.DataFrame, levels_to_match: list, matching_col_suffix: str
+) -> dict:
     """Get the attributes in each of the levels to match, for both the pyramid and the data.
 
     We will then add them in the output matched dataframe.
@@ -204,6 +230,8 @@ def _get_attributes(data: pl.DataFrame, pyramid: pl.DataFrame, levels_to_match: 
         The dataframe with the pyramid to match against.
     levels_to_match : list
         The list of levels to match.
+    matching_col_suffix : str
+        The suffix of the column that we will do the matching on.
 
     Returns
     -------
@@ -216,14 +244,14 @@ def _get_attributes(data: pl.DataFrame, pyramid: pl.DataFrame, levels_to_match: 
         attributes_data = []
         for col in data.columns:
             if col.startswith(level + "_") and col not in [
-                level + "_name",
+                level + matching_col_suffix,
             ]:
                 attributes_data.append(col)
 
         attributes_pyramid = []
         for col in pyramid.columns:
             if col.startswith(level + "_") and col not in [
-                level + "_name",
+                level + matching_col_suffix,
             ]:
                 attributes_pyramid.append(col)
 
@@ -232,7 +260,9 @@ def _get_attributes(data: pl.DataFrame, pyramid: pl.DataFrame, levels_to_match: 
     return attributes
 
 
-def _check_levels(data: pl.DataFrame, pyramid: pl.DataFrame, levels_to_match: list) -> None:
+def _check_levels(
+    data: pl.DataFrame, pyramid: pl.DataFrame, levels_to_match: list, matching_col_suffix: str
+) -> None:
     """Check if the levels to match are present in both data and pyramid dataframes.
 
     Parameters
@@ -243,6 +273,8 @@ def _check_levels(data: pl.DataFrame, pyramid: pl.DataFrame, levels_to_match: li
         The dataframe with the pyramid to match against.
     levels_to_match : list
         The list of levels to match.
+    matching_col_suffix : str
+        The suffix of the column that we will do the matching on.
 
     Raises
     ------
@@ -250,18 +282,20 @@ def _check_levels(data: pl.DataFrame, pyramid: pl.DataFrame, levels_to_match: li
         If any of the levels to match is not present in both data and pyramid dataframes.
     """
     for level in levels_to_match:
-        col_name = level + "_name"
+        col_name = level + matching_col_suffix
         if col_name not in data.columns:
             raise ValueError(f"Level {level} not present in data dataframe.")
         if col_name not in pyramid.columns:
             raise ValueError(f"Level {level} not present in pyramid dataframe.")
 
 
-def _get_levels_to_match(data: pl.DataFrame, pyramid: pl.DataFrame) -> list:
+def _get_levels_to_match(
+    data: pl.DataFrame, pyramid: pl.DataFrame, matching_col_suffix: str
+) -> list:
     """If no levels_to_match are provided, detect the levels to match dynamically.
 
     We find the levels that are present in both data and pyramid dataframes
-    by looking for columns that end with "_name".
+    by looking for columns that end with the matching_col_suffix.
 
     Parameters
     ----------
@@ -269,6 +303,8 @@ def _get_levels_to_match(data: pl.DataFrame, pyramid: pl.DataFrame) -> list:
         The dataframe with the data to be matched.
     pyramid : pl.DataFrame
         The dataframe with the pyramid to match against.
+    matching_col_suffix : str
+        The suffix of the column that we will do the matching on.
 
     Returns
     -------
@@ -279,17 +315,18 @@ def _get_levels_to_match(data: pl.DataFrame, pyramid: pl.DataFrame) -> list:
     levels_in_pyramid = set()
 
     for col in data.columns:
-        if col.endswith("_name"):
-            level = col[:-5]
+        if col.endswith(matching_col_suffix):
+            level = col[: -len(matching_col_suffix)]
             levels_in_data.add(level)
 
     for col in pyramid.columns:
-        if col.endswith("_name"):
-            level = col[:-5]
+        if col.endswith(matching_col_suffix):
+            level = col[: -len(matching_col_suffix)]
             levels_in_pyramid.add(level)
 
     actual_levels = list(levels_in_data.intersection(levels_in_pyramid))
     actual_levels.sort()
+    print(f"Detected levels to match: {actual_levels}")
 
     return actual_levels
 
@@ -303,6 +340,7 @@ def _match_level(
     threshold: int,
     method: str,
     attributes_level: dict,
+    matching_col_suffix: str,
 ) -> tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame]:
     """Do the matching for a particular level, taking into account previously matched levels.
 
@@ -328,6 +366,8 @@ def _match_level(
         The method to use for the matching.
     attributes_level : dict
         The attributes to include for this level, both for data and pyramid.
+    matching_col_suffix : str
+        The suffix of the column that we will do the matching on.
 
     Returns
     -------
@@ -335,8 +375,8 @@ def _match_level(
         The dataframe with the matched data for this level.
     """
     schema_match = (
-        [config.preffix_input_data + target_level + "_name"]
-        + [config.preffix_target_data + target_level + "_name"]
+        [config.preffix_input_data + target_level + matching_col_suffix]
+        + [config.preffix_target_data + target_level + matching_col_suffix]
         + [f"{config.preffix_target_data}{attr}" for attr in attributes_level["pyramid"]]
         + ["score_" + target_level]
         + [f"{config.preffix_input_data}{attr}" for attr in attributes_level["data"]]
@@ -350,13 +390,16 @@ def _match_level(
             method,
             attributes_level,
             schema_match,
+            matching_col_suffix,
         )
 
     list_match = []
     list_no_match_data = []
     list_no_match_pyramid = []
     for row in already_matched.iter_rows(named=True):
-        pyramid_group, data_group = _select_group(data, pyramid, levels_already_matched, row)
+        pyramid_group, data_group = _select_group(
+            data, pyramid, levels_already_matched, row, matching_col_suffix
+        )
         df_match_level_group, unmatched_data_group, unmatched_pyramid_group = _match_level_group(
             pyramid_group,
             data_group,
@@ -365,6 +408,7 @@ def _match_level(
             method,
             attributes_level,
             schema_match,
+            matching_col_suffix,
         )
         if len(df_match_level_group) > 0:
             df_match_level_group = _add_already_matched_levels(row, df_match_level_group)
@@ -396,7 +440,7 @@ def _add_already_matched_levels(row: dict, df_match_row: pl.DataFrame) -> pl.Dat
     Parameters
     ----------
     row : dict
-        The row with the already matched data. It is a dictionary with the column names as keys.
+        The row with the already matched data. It is a dictionary with the matching column as keys.
     df_match_row : pl.DataFrame
         The dataframe with the newly matched data for the current level.
 
@@ -418,6 +462,7 @@ def _match_level_group(
     method: str,
     attributes_level: dict,
     schema_match: list,
+    matching_col_suffix: str,
 ) -> tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame]:
     """Match a level of the data with the corresponding level of the pyramid.
 
@@ -439,13 +484,15 @@ def _match_level_group(
         The attributes to include for this level, both for data and pyramid.
     schema_match: list
         The schema for the matched dataframe.
+    matching_col_suffix : str
+        The suffix of the column that we will do the matching on.
 
     Returns
     -------
     tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame]
         A tuple containing the matched data, unmatched data, and unmatched pyramid data.
     """
-    col_name = level + "_name"
+    col_name = level + matching_col_suffix
 
     data_to_match = {
         row[0]: list(row[1:])
@@ -476,10 +523,14 @@ def _match_level_group(
     else:
         raise ValueError(f"Method {method} not recognized.")
 
-    matched_data_names = df_matches[config.preffix_input_data + level + "_name"].unique()
+    matched_data_names = df_matches[
+        config.preffix_input_data + level + matching_col_suffix
+    ].unique()
     df_unmatched_data = data.filter(~pl.col(col_name).is_in(matched_data_names))
 
-    matched_pyramid_names = df_matches[config.preffix_target_data + level + "_name"].unique()
+    matched_pyramid_names = df_matches[
+        config.preffix_target_data + level + matching_col_suffix
+    ].unique()
     df_unmatched_pyramid = pyramid.filter(~pl.col(col_name).is_in(matched_pyramid_names))
 
     return df_matches, df_unmatched_data, df_unmatched_pyramid
@@ -490,6 +541,7 @@ def _select_group(
     pyramid: pl.DataFrame,
     levels_already_matched: list,
     row: dict,
+    matching_col_suffix: str,
 ) -> tuple[pl.DataFrame, pl.DataFrame]:
     """Select the group of data and pyramid that match the already matched levels.
 
@@ -506,7 +558,9 @@ def _select_group(
     levels_already_matched : list
         The list of levels already matched.
     row : dict
-        The row with the already matched data. It is a dictionary with the column names as keys.
+        The row with the already matched data. It is a dictionary with the matching column as keys.
+    matching_col_suffix : str
+        The suffix of the column that we will do the matching on.
 
     Returns
     -------
@@ -517,10 +571,12 @@ def _select_group(
     relevant_pyramid = pyramid.clone()
     for level in levels_already_matched:
         relevant_data = relevant_data.filter(
-            pl.col(f"{level}_name") == row[f"{config.preffix_input_data}{level}_name"]
+            pl.col(f"{level}{matching_col_suffix}")
+            == row[f"{config.preffix_input_data}{level}{matching_col_suffix}"]
         )
         relevant_pyramid = relevant_pyramid.filter(
-            pl.col(f"{level}_name") == row[f"{config.preffix_target_data}{level}_name"]
+            pl.col(f"{level}{matching_col_suffix}")
+            == row[f"{config.preffix_target_data}{level}{matching_col_suffix}"]
         )
 
     return relevant_pyramid, relevant_data
