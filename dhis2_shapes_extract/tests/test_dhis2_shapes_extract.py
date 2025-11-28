8bf582e36978e6c4089218b50cd9efac6f5c15e6
EMontandon <esteban14m@gmail.com>
2025-11-28 17:50:23 +0100

    test(): check shapes transform works

--- dhis2_shapes_extract/tests/test_dhis2_shapes_extract.py
+++ dhis2_shapes_extract/tests/test_dhis2_shapes_extract.py
@@ -1,6 +1,16 @@
-def test_example():  # noqa: D103
-    assert 1 == 1
+import polars as pl
+from pathlib import Path
+from dhis2_shapes_extract.pipeline import transform_shapes
+
+# import os
+# os.chdir("dhis2_shapes_extract")
+# os.getcwd()
 
 
-def test_example_2():  # noqa: D103
-    assert 1 == 1
+# we are going to test only core functionalities here such as geometry manipulations.
+def test_geometries_lvl_2():  # noqa: D103
+    pyramid = pl.read_parquet(
+        Path(r"dhis2_shapes_extract/tests/test_data/test_pyramid_lvl_2.parquet")
+    )
+    shapes = transform_shapes(pyramid)
+    assert shapes.shape == (5, 5), "Expected 5 rows and 5 columns after transformation"
