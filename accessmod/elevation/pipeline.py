015b890ad42d498308d50d66f16bf66fe6bf67d2
Lea Peligry <lpeligry@bluesquarehub.com>
2025-12-01 10:43:06 +0100

    update merge_tiles and add compute_slope functions

--- accessmod/elevation/pipeline.py
+++ accessmod/elevation/pipeline.py
@@ -1,25 +1,47 @@
 # from pathlib import Path
-from openhexa.sdk import pipeline, current_run # workspace, parameter
-import os 
-from appdirs import user_cache_dir
-import geopandas as gpd
-from shapely.geometry import box
-from typing import List
-from shapely.geometry.base import BaseGeometry
+import os
+from typing import List  # noqa: UP035
+
 import boto3
+import geopandas as gpd
+import rasterio
+import rasterio.merge
+from appdirs import user_cache_dir
 from botocore import UNSIGNED
 from botocore.config import Config
-# import processing
-# import rasterio
-# from rasterio.crs import CRS
+from openhexa.sdk import current_run, pipeline  # workspace, parameter
+from osgeo import gdal
+from shapely.geometry import box
+from shapely.geometry.base import BaseGeometry
 
+# import processing
+
+# from rasterio.crs import CRS
 
 
 WORK_DIR = os.path.join(user_cache_dir("accessmod"), "elevation")
 # DATA_PATH = os.path.join(workspace.files_path, "pipelines/accessmod/elevation/data")
 DATA_PATH = "tmp/accessmod/elevation/data"
 
-print('string')
+RASTERIO_DEFAULT_PROFILE = {
+    "driver": "GTiff",
+    "tiled": True,
+    "blockxsize": 256,
+    "blockysize": 256,
+    "compress": "zstd",
+    "predictor": 2,
+    "num_threads": "all_cpus",
+}
+
+GDAL_CREATION_OPTIONS = [
+    "TILED=TRUE",
+    "BLOCKXSIZE=256",
+    "BLOCKYSIZE=256",
+    "COMPRESS=ZSTD",
+    "PREDICTOR=2",
+    "NUM_THREADS=ALL_CPUS",
+]
+
 
 @pipeline("landcover")
 def generate_land_cover(target_geometry):
@@ -35,14 +57,16 @@ def generate_land_cover(target_geometry):
     # Download data 
     tiles = download_tiles(name_list=tiles_name, output_path=DATA_PATH)
 
-    # Merge tiles and crop result using the target geometry
-    landcover = merge_tiles(tiles=tiles, target_geom=target_geometry)
+    # Merge tiles 
+    mosaic = merge_tiles(tiles=tiles, output_file=DATA_PATH + "Copernicus_elevation_merge.tif")
+
+    # Compute slope
+    slope = compute_slope(input_file=mosaic, output_file=DATA_PATH + "copernicus_elevation_slope.tif")
+
+    # Add cleaning steps -> remove raw and intermediate files from the output folder even if it's in tmp ??
 
 
-    return None
-
-
-
+#########################
 def create_global_grid_name(latitude_spacing: int, longitude_spacing: int, pattern: str) -> gpd.GeoDataFrame:
     
     tiles = []
@@ -60,13 +84,10 @@ def create_global_grid_name(latitude_spacing: int, longitude_spacing: int, patte
     return gpd.GeoDataFrame(tiles, crs="EPSG:4326")
 
 
+#########################
 def retrieve_tiles(target_geom: BaseGeometry) -> List[str]:
-
-    """
-    Return a list of paths to files of interest 
-    """
     
-    if target_geom.crs != 'EPSG:4326':
+    if target_geom.crs != "EPSG:4326":
         target_geom.to_crs(4326)
 
     # Create a global grid    
@@ -79,6 +100,7 @@ def retrieve_tiles(target_geom: BaseGeometry) -> List[str]:
     return selected_tiles["name"].tolist()
 
 
+#########################
 def download_tiles(name_list: List[str], output_path: str):
 
     s3 = boto3.client('s3', region_name="eu-central-1", config=Config(signature_version=UNSIGNED))
@@ -90,31 +112,43 @@ def download_tiles(name_list: List[str], output_path: str):
     return name_list
 
 
-def merge_tiles(tiles: List[str], output_file: str, overwrite: bool = False):
-
-    # fs = utils.filesystem(output_file)
-    # if fs.isfile(dst_file) and not overwrite:
-    #     current_run.log_info(f"File {output_file} already exists.")
-    #     return output_file
+#########################
+def merge_tiles(tiles: List[str], output_file: str) -> str:
 
     with rasterio.open(tiles[0]) as src:
         meta = src.meta.copy()
 
     mosaic, dst_transform = rasterio.merge.merge(tiles)
-    # meta.update(RASTERIO_DEFAULT_PROFILE) 
-    # RASTERIO_DEFAULT_PROFILE = {
-                                # "driver": "GTiff",
-                                # "tiled": True,
-                                # "blockxsize": 256,
-                                # "blockysize": 256,
-                                # "compress": "zstd",
-                                # "predictor": 2,
-                                # "num_threads": "all_cpus",
-                                # }
+    meta.update(RASTERIO_DEFAULT_PROFILE) 
     meta.update(transform=dst_transform, height=mosaic.shape[1], width=mosaic.shape[2])
 
     with rasterio.open(output_file, "w", **meta) as dst:
         dst.write(mosaic)
+
     current_run.log_info(f"Merged {len(tiles)} tiles into mosaic {output_file}.")
 
     return output_file
+
+
+#########################
+def compute_slope(input_file: str, output_file: str) -> str:
+
+    src_ds = gdal.Open(input_file)
+    scale = None
+    if not src_ds.GetSpatialRef().IsProjected():
+        # because source ref system is EPSG:4326 + slope computed in meters 
+        scale = 111120   
+        # If it was in feet: scale = 370400
+
+    options = gdal.DEMProcessingOptions(
+        format="GTiff",
+        scale=scale,    # ratio of vertical units to horizontal
+        slopeFormat="degree",
+        creationOptions=GDAL_CREATION_OPTIONS,
+    )
+    gdal.DEMProcessing(output_file, input_file, "slope", options=options)
+    
+    return output_file
+
+
+
